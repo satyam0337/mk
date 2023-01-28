73a9ad34c90e4c1b6fa5144d12688d5522e5520d
Satyam kumar <105915649+satyam0337@users.noreply.github.com>
2023-01-28 20:52:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = async () => {
-  mongoose.set("strictQuery", false);
+  mongoose.set("strictQuery", true);
   return mongoose.connect('mongodb+srv://test:test@cluster0.mlcnreq.mongodb.net/?retryWrites=true&w=majority');
 };
 module.exports = connect;
